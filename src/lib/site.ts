5ad20579e3874806cea154c72658e33eda738d8a
EdyDoto <39072231+Ederhdz@users.noreply.github.com>
2026-02-05 14:21:51 -0600

    feat(components): add reusable static brand slider with Glide.js

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -3,3 +3,24 @@ export const SITE = {
   legalName: 'Empresa®',
   slogan: 'Distribuimos las mejores marcas de equipo médico',
 };
+
+export const BRANDS = [
+  { name: "Ambu", logo: "Ambu.png" },
+  { name: "Astar", logo: "Astar.png" },
+  { name: "Cadwell", logo: "Cadwell.png" },
+  { name: "Deymed", logo: "Deymed.png" },
+  { name: "Electrocap", logo: "Electrocap.png" },
+  { name: "Esaote", logo: "Esaote.png" },
+  { name: "Luciole medical", logo: "Luciole medical.png" },
+  { name: "Hasomed", logo: "Hasomed - Logotipo.png" },
+  { name: "NE Neuroelectrics", logo: "NE.png" },
+  { name: "Physiomed", logo: "Physiomed.png" },
+  { name: "Rimed", logo: "Rimed.png" },
+  { name: "Hasomed Rehacom", logo: "Rehacom.png" },
+  { name: "Samsung Medison", logo: "Samsung Medison.png" },
+  { name: "Spes medica", logo: "spes.png" },
+  { name: "Spine Guard", logo: "Spine Guard - logotipo.png" },
+  { name: "Syneika", logo: "Syneika - Logotipo.png" },
+  { name: "tVNS", logo: "tVNS Health - Logotipo.png" },
+  { name: "Weaver", logo: "Weaver.png" }, 
+];
---
+++ src/scripts/init-sliders.ts
@@ -0,0 +1,20 @@
+import { initGlide } from '@lib/glide';
+
+document.addEventListener('DOMContentLoaded', () => {
+   initGlide('#BrandSlider', {
+    type: 'carousel',
+    perView: 8,
+    gap: 0,
+    autoplay: 2000, // 2 segundos
+    hoverpause: true,
+    rewind: false, // para que sea infinito
+    breakpoints: {
+      1024: {
+        perView: 4
+      },
+      640: {
+        perView: 2
+      }
+    }
+  });
+});
