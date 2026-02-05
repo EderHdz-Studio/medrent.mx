c44136f86379630c6bbc9e1e368d84d803b495d2
EdyDoto <39072231+Ederhdz@users.noreply.github.com>
2026-02-05 15:59:52 -0600

    feat(components): add reusable static promotion slider section

--- src/scripts/init-sliders.ts
+++ src/scripts/init-sliders.ts
@@ -17,4 +17,21 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   });
+
+  initGlide('#PromotionSlider', {
+    type: 'slider',
+    perView: 1,
+    gap: 0,
+    // autoplay: 2000, // 2 segundos
+    hoverpause: true,
+    //rewind: false, // para que sea infinito
+    breakpoints: {
+      1024: {
+        perView: 1
+      },
+      640: {
+        perView: 1
+      }
+    }
+  });
 });
