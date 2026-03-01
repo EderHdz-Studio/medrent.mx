c9cd38686cf50bc1a29166f0bbe0cbeed4ed223c
EdyDoto <39072231+Ederhdz@users.noreply.github.com>
2026-03-01 16:46:43 -0600

    feat(plp): add multi-select filtering using $in operator

--- src/server/api/products.ts
+++ src/server/api/products.ts
@@ -6,58 +6,39 @@ const PLP_POPULATE = `
     ?populate[gallery]=true
     &populate[brand]=true
 `.replace(/\s+/g, '');
-
 export async function getProducts(filters?: {
-  brand?: string;
-  category?: string;
-  subcategory?: string;
+  brands?: string[];
+  categories?: string[];
+  subcategories?: string[];
 }): Promise<Product[]> {
-  let filterQuery = "";
 
-  if (filters?.brand) {
-    filterQuery += `&filters[brand][slug][$eq]=${filters.brand}`;
-  }
+  const params = new URLSearchParams();
 
-  if (filters?.category) {
-    filterQuery += `&filters[subcategory][category][slug][$eq]=${filters.category}`;
-  }
+  filters?.brands?.forEach((brand, index) => {
+    params.append(
+      `filters[brand][slug][$in][${index}]`,
+      brand
+    );
+  });
 
-  if (filters?.subcategory) {
-    filterQuery += `&filters[subcategory][slug][$eq]=${filters.subcategory}`;
-  }
+  filters?.categories?.forEach((category, index) => {
+    params.append(
+      `filters[subcategory][category][slug][$in][${index}]`,
+      category
+    );
+  });
 
-  const res = await strapiFetch(
-    `/products${PLP_POPULATE}${filterQuery}`
-  );
+  filters?.subcategories?.forEach((subcategory, index) => {
+    params.append(
+      `filters[subcategory][slug][$in][${index}]`,
+      subcategory
+    );
+  });
 
-//   return res.data;
+  const queryString = params.toString();
+  const url = `/products${PLP_POPULATE}${queryString ? `&${queryString}` : ""}`;
+
+  const res = await strapiFetch(url);
+
   return res.data.map(mapStrapiProduct);
 }
-
-
-const PDP_POPULATE = `
-    &populate[gallery]=true
-    &populate[documents]=true
-    &populate[benefit][populate][icon]=true
-    &populate[faqItem]=true
-    &populate[technicalSheet][populate][technicalFeature]=true
-    &populate[technicalSpecifications]=true
-    &populate[relatedConsumables][populate][gallery]=true
-    &populate[relatedProducts][populate][gallery]=true
-    &populate[relatedProducts][populate][brand]=true
-    &populate[variants]=true
-    &populate[subcategory]=true
-    &populate[brand]=true
-`.replace(/\s+/g, '');
-
-export async function getProductBySlug(slug: string): Promise<Product | null> {
-  const res = await strapiFetch(
-    `/products?filters[slug][$eq]=${slug}${PDP_POPULATE}`
-  );
-
-  if (!res.data.length) return null;
-
-//   return res.data[0];
-  return mapStrapiProduct(res.data[0]);
-}
-
