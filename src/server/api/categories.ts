6e7b54219724ff2db92d5a8004ee55871112e935
PazMedinaGuz <pazmedinaupn@gmail.com>
2026-04-21 09:43:36 -0600

    fix(api): fix categories fetch and add isActive filter

--- src/server/api/categories.ts
+++ src/server/api/categories.ts
@@ -2,13 +2,37 @@ import { strapiFetch } from "../strapiClient";
 
 export async function getCategories() {
   const res = await strapiFetch(
-    "/categories?populate[subcategories][populate][image]=true&populate[subcategories][populate][products]=true&populate[heroImage]=true&populate[listImage]=true",
+    "/categories?populate[subcategories][populate][image]=true&populate[subcategories][populate][products]=true&populate[heroImage]=true&populate[listImage]=true&filters[isActive][$eq]=true",
   );
 
-  return res.data;
-  return res.data.map((c: any) => ({
-    id: c.id,
-    name: c?.name ?? "",
-    slug: c?.slug ?? "",
-  }));
+  // Mapear los datos correctamente
+  return (
+    res.data?.map((c: any) => {
+      const attrs = c.attributes || c;
+      return {
+        id: c.id,
+        name: attrs.name ?? "",
+        slug: attrs.slug ?? "",
+        isActive: attrs.isActive ?? true,
+        listImage: attrs.listImage?.url ?? null,
+        heroImage: attrs.heroImage?.url ?? null,
+        subcategories:
+          attrs.subcategories?.data?.map((sub: any) => {
+            const subAttrs = sub.attributes || sub;
+            return {
+              id: sub.id,
+              name: subAttrs.name ?? "",
+              slug: subAttrs.slug ?? "",
+              image: subAttrs.image?.url ?? null,
+              products:
+                subAttrs.products?.data?.map((prod: any) => ({
+                  id: prod.id,
+                  name: prod.attributes?.name ?? prod.name,
+                  slug: prod.attributes?.slug ?? prod.slug,
+                })) ?? [],
+            };
+          }) ?? [],
+      };
+    }) ?? []
+  );
 }
